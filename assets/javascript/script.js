7aa8403aaed9141971d6256bbcb6599aed9f64fd
Leonardo Santos <leonardojosantos@gmail.com>
2024-02-23 23:18:00 -0300

    Implemented form validation and popup display in JS

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,73 +1,104 @@
-(function () {
-    const htmlElements = {
-        formElement: document.querySelector('form'),
-        userName: document.querySelector('[js_data_name]'),
-        userEmail: document.querySelector('[js_data_email]'),
-        userMessage: document.querySelector('[js_data_message]'),
-        buttonForm: document.querySelector('[js_data_form_button]'),
-    };
+const htmlElements = {
+    formElement: document.querySelector('form'),
+    formUserName: document.querySelector('[js_data_name]'),
+    formUserEmail: document.querySelector('[js_data_email]'),
+    formUserMessage: document.querySelector('[js_data_message]'),
+    formButton: document.querySelector('[js_data_form_button]'),
+    popupContainer: document.querySelector('[js_data_popup_container]'),
+    popupMessage: document.querySelector('[js_data_popup_message]'),
+    popupButon: document.querySelector('[js_data_popup_button]'),
+};
 
-    const adresses = {
-        api: 'https://api.sheetmonkey.io/form/nSQ3FU9Kaomuj8BHtUQncR',
-        errorPage: 'https://leojosants.github.io/stackx_projeto_developer_portfolio/error_page.html',
-        successPage: 'https://leojosants.github.io/stackx_projeto_developer_portfolio/success_page.html',
-    };
+const adresses = {
+    api: 'https://api.sheetmonkey.io/form/nSQ3FU9Kaomuj8BHtUQncR',
+    errorPage: 'https://leojosants.github.io/stackx_projeto_developer_portfolio/error_page.html',
+    successPage: 'https://leojosants.github.io/stackx_projeto_developer_portfolio/success_page.html',
+};
 
-    const renderButton = {
-        image: '<img class="c-section-contact__button--animation" src="./assets/images/loading.png" alt="Loading">',
-        message: 'SEND MESSAGE'
-    };
+const renderButton = {
+    image: `
+        <img
+            class="c-section-contact__button--animation"
+            src="./assets/images/loading.png"
+            alt="Loading"
+        />
+    `,
+    message: 'SEND MESSAGE'
+};
 
-    const addLoad = () => {
-        htmlElements.buttonForm.innerHTML = renderButton.image;
-    };
+const displayPopup = (message) => {
+    htmlElements.popupContainer.style.display = 'block';
+    htmlElements.popupMessage.innerHTML = message;
+    htmlElements.popupButon.addEventListener('click', () => { htmlElements.popupContainer.style.display = 'none'; });
+};
 
-    const removedLoad = () => {
-        htmlElements.buttonForm.innerHTML = renderButton.message;
-    };
+const addLoadImage = () => {
+    htmlElements.formButton.innerHTML = renderButton.image;
+};
 
-    const delaySuccess = () => {
-        setTimeout(() => {
-            window.location.href = adresses.successPage;
-            removedLoad();
-        }, 1000);
+const removedLoadImage = () => {
+    htmlElements.formButton.innerHTML = renderButton.message;
+};
+
+const delaySuccess = () => {
+    setTimeout(() => {
+        window.location.href = adresses.successPage;
+        removedLoadImage();
+    }, 1000);
+};
+
+const delayError = () => {
+    setTimeout(() => {
+        window.location.href = adresses.errorPage;
+        removedLoadImage();
+    }, 1000);
+};
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+    const userNameValue = htmlElements.formUserName.value;
+    const userEmailValue = htmlElements.formUserEmail.value;
+    const userMessageValue = htmlElements.formUserMessage.value;
+    
+    
+    if (!userNameValue) {
+        displayPopup('NAME field is empty!');
+        return;
     }
 
-    const delayError = () => {
-        setTimeout(() => {
-            window.location.href = adresses.errorPage;
-            removedLoad();
-        }, 1000);
+    if (!userEmailValue) {
+        displayPopup('EMAIL field is empty!');
+        return;
     }
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        addLoad();
+    if (!userMessageValue) {
+        displayPopup('MESSAGE field is empty!');
+        return;
+    }
 
-        const userNameValue = htmlElements.userName.value;
-        const userEmailValue = htmlElements.userEmail.value;
-        const userMessageValue = htmlElements.userMessage.value;
+    addLoadImage();
 
-        fetch(adresses.api,
-            {
-                method: 'post',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(
-                    {
-                        name: userNameValue,
-                        email: userEmailValue,
-                        message: userMessageValue,
-                        date: new Date().toLocaleString(),
-                    }
-                ),
+    htmlElements.formButton.style.pointerEvents = 'none';
+
+    fetch(adresses.api,
+        {
+            method: 'post',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
             },
-        )
-            .then(() => delaySuccess())
-            .catch(() => delayError());
-    };
+            body: JSON.stringify(
+                {
+                    name: userNameValue,
+                    email: userEmailValue,
+                    message: userMessageValue,
+                    date: new Date().toLocaleString(),
+                }
+            ),
+        },
+    )
+        .then(() => delaySuccess())
+        .catch(() => delayError());
+};
 
-    htmlElements.formElement.addEventListener('submit', handleSubmit);
-})();
+htmlElements.formElement.addEventListener('submit', handleSubmit);
